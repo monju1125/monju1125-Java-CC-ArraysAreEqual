fa98a90b8388cb45005a7ce5a7b20b288ebc93ae
monju1125 <monju1125@github.com>
2026-06-08 04:09:13 +0000

    Test run: 3/3 passed, 0 failed, 0 skipped - 2026-06-08T04:09:13.624Z

--- src/main/java/ArraysAreEqual.java
+++ src/main/java/ArraysAreEqual.java
@@ -20,9 +20,8 @@ public class ArraysAreEqual {
             for(int i=0; i < a.length; i++){
                     if(a[i] != b[i]){
                             return false;
-                    }
-                return true;
+                    } 
             }
-        return false;
+        return true;
     }
 }
